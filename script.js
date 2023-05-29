758e0df16504d66d89d4c1b919d95d07ccb9ef30
Suansh Budhiraja <43946155+ishaansharma750@users.noreply.github.com>
2023-05-30 00:35:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+const screenSize =  document.getElementById("sizeInfo");
+screenSize.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+document.body.append(screenSize);
+
+
+
+
+window.onresize = function(){
+    screenSize.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+}
